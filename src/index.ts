140bdb79631ccb084574b0a8ba749c2b8daaf1ff
Kevin Grant <kevingrant@Kevins-MacBook-Pro.local>
2023-04-22 00:06:27 -0400

    Revert "Try and use this thing"
    This reverts commit 0d4604c0f6e4d301d2024cf250cdf46e454ba366.

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,11 @@
-import { AccessoryPlugin, HAP, Logging } from 'homebridge';
-import axios from 'axios';
+import { API } from 'homebridge';
 
-interface LedState {
-  on: boolean;
-}
+import { PLATFORM_NAME } from './settings';
+import { ExampleHomebridgePlatform } from './platform';
 
-class MicroPythonLEDAccessory implements AccessoryPlugin {
-  private readonly log: Logging;
-  private readonly apiBaseUrl: string;
-  private readonly lightService: HAP.Service;
-  private readonly informationService: HAP.Service;
-
-  constructor(log: Logging, config: any, api: HAP.API) {
-    this.log = log;
-    this.apiBaseUrl = config.apiBaseUrl;
-
-    // Create the information service for the accessory
-    this.informationService = new api.hap.Service.AccessoryInformation()
-      .setCharacteristic(api.hap.Characteristic.Manufacturer, 'Custom')
-      .setCharacteristic(api.hap.Characteristic.Model, 'MicroPython LED')
-      .setCharacteristic(api.hap.Characteristic.SerialNumber, '001');
-
-    // Create the light service for the accessory
-    this.lightService = new api.hap.Service.Lightbulb(config.name);
-
-    // Set up the "On" characteristic for the light service
-    this.lightService.getCharacteristic(api.hap.Characteristic.On)
-      .onSet(this.setOn.bind(this));
-  }
-
-  async setOn(value: boolean) {
-    try {
-      const ledState: LedState = {
-        on: value
-      };
-      const response = await axios.post(`${this.apiBaseUrl}/led`, ledState);
-      const newState = response.data.on;
-      this.log.info(`Set LED state to: ${newState ? 'ON' : 'OFF'}`);
-    } catch (error: any) {
-      if (error.response) {
-        this.log.error(`Error setting LED state: ${error.response.status} ${error.response.statusText}`);
-      } else {
-        this.log.error('Error setting LED state:', error.message);
-      }
-    }
-  }
-
-  getServices(): HAP.Service[] {
-    return [this.informationService, this.lightService];
-  }
-}
-
-export default (homebridge: HAP.API): void => {
-  homebridge.registerAccessory('homebridge-micropython-led', 'MicroPythonLED', MicroPythonLEDAccessory);
+/**
+ * This method registers the platform with Homebridge
+ */
+export = (api: API) => {
+  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
 };
