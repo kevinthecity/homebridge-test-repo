1a437b09dd3eff8fa2508b180e4ebd613ae0cd7c
Kevin Grant <kevingrant@Kevins-MacBook-Pro.local>
2023-04-22 00:36:56 -0400

    Make it more led purposed

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,34 +1,34 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
-
 import { ExampleHomebridgePlatform } from './platform';
+import axios from 'axios';
+
+interface LedState {
+  on: boolean;
+}
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
-  private service: Service;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+export class ExamplePlatformAccessory {
+  
+  private service: Service;
+  private readonly apiBaseUrl: string;
 
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
+    this.apiBaseUrl = "http://192.168.7.166:8080/"
+
     // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Custom')
+      .setCharacteristic(this.platform.Characteristic.Model, 'MicroPython LED')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, '001');
 
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
@@ -43,65 +43,33 @@ export class ExamplePlatformAccessory {
 
     // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+      .onSet(this.setOn.bind(this));                // SET - bind to the `setOn` method below
+      // .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
   }
 
-  /**
+    /**
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
   async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+    try {
+      const ledState: LedState = {
+        on: value as boolean
+      };
+      const response = await axios.post(`${this.apiBaseUrl}/led`, ledState);
+      const newState = response.data.on;
+      console.log(`Set LED state to: ${newState ? 'ON' : 'OFF'}`);
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+      this.platform.log.debug('Set Characteristic On ->', value);
+
+    } catch (error: any) {
+      if (error.response) {
+        console.error(`Error setting LED state: ${error.response.status} ${error.response.statusText}`);
+      } else {
+        console.error('Error setting LED state:', error.message);
+      }
+    }
   }
-
   /**
    * Handle the "GET" requests from HomeKit
    * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
@@ -115,27 +83,28 @@ export class ExamplePlatformAccessory {
    * @example
    * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
    */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+  // async getOn() {
+  //   try {
+  //     const response = await axios.get(`${this.apiBaseUrl}/led`);
+  //     const newState = response.data.on;
+  //     if (newState !== this.on) {
+  //       this.on = newState;
+  //       console.log(`LED state changed to: ${newState ? 'ON' : 'OFF'}`);
+  //     }
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+  //     this.platform.log.debug('Get Characteristic On ->', isOn);
 
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+  //     return newState;
+  //   } catch (error: any) {
+  //     if (error.response) {
+  //       console.error(`Error getting LED state: ${error.response.status} ${error.response.statusText}`);
+  //     } else {
+  //       console.error('Error getting LED state:', error.message);
+  //     }
+  //   }
 
-    return isOn;
-  }
+  //       // if you need to return an error to show the device as "Not Responding" in the Home app:
+  //   // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
-  }
-
+  // }
 }
